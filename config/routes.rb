6a595325f6d52d335333289d1e851dea89f576be
Rakesh.P.D <errakeshpd@gmail.com>
2014-05-27 17:13:16 +0530

    main page root ulter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'profile/index'
+  root 'profile#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
